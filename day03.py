bb285b5cf1ecfda1e5b22506a131a3badfb84678
Greg Inabinet <Gregory.Inabinet@samtec.com>
2021-12-03 09:18:10 -0500

    day03 solution for part 2

--- day03.py
+++ day03.py
@@ -16,6 +16,46 @@ def calculatePowerConsumption(report):
     return powerComp, gamma, epsilon
 
 
+def filterList(values, pos=0, mostCommon=True):
+    zeros = []
+    ones = []
+    for row in values:
+        if int(row[pos]):
+            ones.append(row)
+        else:
+            zeros.append(row)
+
+    out = None
+    if mostCommon:
+        if len(zeros) > len(ones):
+            out = zeros
+        else:
+            out = ones
+    else:
+        if len(ones) < len(zeros):
+            out = ones
+        else:
+            out = zeros
+
+    if len(out) == 1:
+        return out[0]
+    else:
+        cmdCnt = len(values[0])
+        pos += 1
+        pos %= cmdCnt
+        return filterList(out, pos=pos, mostCommon=mostCommon)
+
+
+def calculateLifeSupportRating(report):
+    oxgen = filterList(report, mostCommon=True)
+    oxgen = int(oxgen, 2)
+    co2scrub = filterList(report, mostCommon=False)
+    co2scrub = int(co2scrub, 2)
+    lifeRate = oxgen * co2scrub
+    return lifeRate, oxgen, co2scrub
+
+
+
 # Example
 tmp = """\
 00100
@@ -34,9 +74,11 @@ tmp = """\
 exampleInputs = [i.strip() for i in tmp.split('\n')]
 
 print(calculatePowerConsumption(exampleInputs))
+print(calculateLifeSupportRating(exampleInputs))
 
 
 # Part 1
 print(calculatePowerConsumption(inputs))
 
 # Part 2
+print(calculateLifeSupportRating(inputs))
