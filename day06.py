89b46069849f03dd00a97744cd6291905688ec86
Greg Inabinet <Gregory.Inabinet@samtec.com>
2021-12-06 09:01:55 -0500

    day06 solution for part 1

--- day06.py
+++ day06.py
@@ -3,6 +3,36 @@ with open('inputs/day06','r') as f:
     inputs = [i.strip() for i in f]
 
 
+def calcNumberFish(initialPopulation, nDays=80):
+    # fish arrays
+    oldFish = [0 for _ in range(7)]
+    newFish = [0 for _ in range(9)]
+
+    # initialalize oldFish array from initial population
+    for fish in initialPopulation:
+        oldFish[fish] += 1
+
+    for day in range(nDays):
+        oldFishSpawnDay = day % 7
+        newFishSpawnDay = day % 9
+
+        # find count of new fish spawned from new fish population (9-day reproduction)
+        babyFishFromNew = newFish[newFishSpawnDay]
+
+        # find count of new fish spawned from old fish population (7-day reproduction)
+        babyFishFromOld = oldFish[oldFishSpawnDay]
+
+        # these fish will now re-spawn with the old population
+        oldFish[oldFishSpawnDay] += babyFishFromNew
+
+        # these fish will now re-spawn with the new population
+        newFish[newFishSpawnDay] += babyFishFromOld
+
+    nOld = sum(oldFish)
+    nNew = sum(newFish)
+    total = nOld + nNew
+    return(total)
+
 
 # Example
 tmp = """\
@@ -11,44 +41,13 @@ tmp = """\
 exampleInputs = [i.strip() for i in tmp.split('\n')]
 
 fishes = [int(f) for f in exampleInputs[0].split(',')]
-#fishType = {f:0 for f in range(-2,7)}
+print(calcNumberFish(fishes))
 
-# fish arrays
-oldFish = [0 for f in range(7)]
-newFish = [0 for f in range(9)]
-
-# initial array
-for fish in fishes:
-    oldFish[fish] += 1
-
-#print(0, oldFish)
-#print(0, newFish)
-#print()
-
-for day in range(80):
-    oldFishSpawnDay = day % 7
-    newFishSpawnDay = day % 9
-
-    # find count of new fish spawned from new fish population (9-day reproduction)
-    babyFishFromNew = newFish[newFishSpawnDay]
-
-    # find count of new fish spawned from old fish population (7-day reproduction)
-    babyFishFromOld = oldFish[oldFishSpawnDay]
-
-    # these fish will now re-spawn with the old population
-    oldFish[oldFishSpawnDay] += babyFishFromNew
-
-    # these fish will now re-spawn with the new population
-    newFish[newFishSpawnDay] += babyFishFromOld
-
-    #print(oldFishSpawnDay+1,oldFish)
-    #print(newFishSpawnDay+1,newFish)
-    #print()
-print(sum(oldFish)+sum(newFish))
 
 
 # Part 1
-
+fishes = [int(f) for f in inputs[0].split(',')]
+print(calcNumberFish(fishes))
 
 
 # Part 2
