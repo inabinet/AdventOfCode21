76bea4e60d181ecbb9c578c49caa7d25be50d366
Greg Inabinet <Gregory.Inabinet@samtec.com>
2021-12-10 10:14:53 -0500

    day10 solution for part 2

--- day10.py
+++ day10.py
@@ -1,3 +1,4 @@
+from numpy import median
 
 with open('inputs/day10','r') as f:
     inputs = [i.strip() for i in f]
@@ -11,35 +12,52 @@ close = {
 }
 
 
-points = {
-    ')': 3,
-    ']': 57,
-    '}': 1197,
-    '>': 25137,
-}
-
-
-def getCorruptedDict(lines):
+def getCorruptedAndEndingDicts(lines):
     corrupted = {}
+    endings = {}
     for i,line in enumerate(lines):
         stack = []
         for char in line:
             if char in close.keys():    # keys are the start characters :-|
                 stack.append(char)
             else:
-                if len(stack) == 0:
-                    print('ummm...')
                 strt = stack.pop()
                 if close[strt] != char:
                     corrupted[i] = char
                     break
-    return corrupted
+        if i not in corrupted:
+            endings[i] = [close[i] for i in reversed(stack)]
+    return corrupted, endings
 
 
-def calculateScore(lines):
-    corrupted = getCorruptedDict(lines)
+def calculateSyntaxErrorScore(lines):
+    points = {
+        ')': 3,
+        ']': 57,
+        '}': 1197,
+        '>': 25137,
+    }
+    corrupted, _ = getCorruptedAndEndingDicts(lines)
     vals = [points[c] for c in corrupted.values()]
     return sum(vals)
+
+
+def calculateAutocompleteScore(lines):
+    points = {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4,
+    }
+    _, endings = getCorruptedAndEndingDicts(lines)
+    scores = []
+    for ending in endings.values():
+        score = 0
+        for char in ending:
+            score *= 5
+            score += points[char]
+        scores.append(score)
+    return int(median(scores))
 
 
 
@@ -58,11 +76,13 @@ tmp = """\
 
 exampleInputs = [i.strip() for i in tmp.split('\n')]
 
-print(calculateScore(exampleInputs))
+print(calculateSyntaxErrorScore(exampleInputs))
+print(calculateAutocompleteScore(exampleInputs))
 
 
 # Part 1
-print(calculateScore(inputs))
+print(calculateSyntaxErrorScore(inputs))
 
 
 # Part 2
+print(calculateAutocompleteScore(inputs))
